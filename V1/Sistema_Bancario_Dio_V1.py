063b8205ad506cdf4755d14ffa2720eebd03f6b5
HyogoMoura <diogofilipilsht@poli.br>
2024-08-28 14:41:02 -0300

    Adicionado exibição de data e hora simples nas operacoes

--- V1/Sistema_Bancario_Dio_V1.py
+++ V1/Sistema_Bancario_Dio_V1.py
@@ -1,5 +1,5 @@
-from datetime import date, datetime
-data_hora_atual = datetime.now()
+from datetime import date, datetime, timedelta
+
 menu = """
 
 Digite [1] para Depositar
@@ -11,6 +11,7 @@ Digite [4] para Sair
 saldo = 0
 limite = 500
 extrato = ""
+data_hora_atual = ""
 numero_saques = 0
 LIMITE_SAQUES = 3
 
@@ -22,8 +23,9 @@ while True:
         valor = float(input("Informe o valor do depósito: "))
 
         if valor > 0:
+            data_hora_atual = datetime.now()
             saldo += valor
-            extrato += f"Depósito: R$ {valor:.2f}\n"
+            extrato += f"Depósito: R$ {valor:.2f} --- {data_hora_atual.strftime("%d/%m/%Y %H:%M")}\n "
 
         else:
             print("Operação falhou! O valor informado é inválido.")
@@ -37,6 +39,8 @@ while True:
 
         excedeu_saques = numero_saques >= LIMITE_SAQUES
 
+
+
         if excedeu_saldo:
             print("Operação falhou! Você não tem saldo suficiente.")
 
@@ -48,7 +52,8 @@ while True:
 
         elif valor > 0:
             saldo -= valor
-            extrato += f"Saque: R$ {valor:.2f}\n"
+            data_hora_atual = datetime.now()
+            extrato += f"Saque: R$ {valor:.2f}----- {data_hora_atual.strftime("%d/%m/%Y %H:%M")}\n"
             numero_saques += 1
 
         else:
@@ -56,6 +61,7 @@ while True:
 
     elif opcao == "3":
         print("\n================ EXTRATO ================")
+        print(f"\n================{data_hora_atual.now().date()}================")
         print("Não foram realizadas movimentações." if not extrato else extrato)
         print(f"\nSaldo: R$ {saldo:.2f}")
         print("==========================================")
